c1d5661c44734a4c654a359b4935aad5d8396099
Ausha Homer <Aushahomer@weber.mail.edu>
2023-11-27 19:31:30 -0700

    done

---
+++ array_1.py
@@ -0,0 +1,43 @@
+print ('Ausha Homer')
+
+
+cars =['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep','Chevy','GMC',] 
+print (cars)
+
+print (len(cars))
+
+cars.append('Buick')
+
+print (cars)
+
+print (cars[3])
+
+cars.insert(2,'Toyota')
+
+print (cars)
+
+cars.pop(5)
+
+print (cars)
+
+cars.sort()
+
+print (cars)
+
+cars.sort(reverse=True)
+
+print (cars)
+
+my_array_length = len(cars)
+
+
+array_string = "The length of my array is: "
+
+print (array_string + str(my_array_length))
+
+
+#``problem 16. create a variable called my_array_length with a value of the cars array length (spelling, capitilization, and spaces matter)`
+
+#`problem 17. create a variable called array_string with a value of 'The length of my array is ' (spelling, capitilization, and spaces matter)`
+
+#`problem 18. print array_string concatenated with my_array_length to the console.`
