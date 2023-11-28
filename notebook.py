628248e7dbc99deca2c2cc808de8f86e6da3602b
Ausha Homer <Aushahomer@weber.mail.edu>
2023-11-27 19:31:14 -0700

    done

---
+++ notebook.py
@@ -0,0 +1,19 @@
+#notebook.py
+#bird = input("Choose a color (red, yellow, green, blue):")
+#if colorchoice == 'red' or colorchoice == 'green':
+   # evenColor = input("Choose a number (1,2,5,6,):")
+    #if evenColor == '1' or '5':
+     #   finOdNum = input("Choose a number,(3,4,7,8")
+    #
+    # else :
+       # finEvNum = input("Choose a number (1,2,5,6):")
+      #  lastNum = 0
+#(input("You found a Cootie Catcher!\nAsk your Cootie Catcher a question:"))       
+
+#Error type needed for text. TypeError and ValueError, Excpt
+#def __init__(self):
+        #self.color= input("Pick a one color, red, yellow, blue, green:")
+        #self.evNum = input ("Pick a number (3,4,7,8):")
+        #self.odNum = input("Pick a number (1,2,5,6):")
+        #self.lastNum = input ("One last step! Choose wisely!")
+        
